433c221a650a8e0031e468445ba0e5df75060f72
kadin <kadinpadams@gmail.com>
2024-09-17 20:14:58 -0600

    start

--- week01/teach/Divisors.cs
+++ week01/teach/Divisors.cs
@@ -16,9 +16,13 @@ public static class Divisors {
     /// </summary>
     /// <param name="number">The number to find the divisor</param>
     /// <returns>List of divisors</returns>
-    private static List<int> FindDivisors(int number) {
+     private static List<int> FindDivisors(int number) {
         List<int> results = new();
-        // TODO problem 1
+        for (int i = 1; i < number; ++i) {
+            if (number % i == 0) {
+                results.Add(i);
+            }
+        }
         return results;
     }
 }
